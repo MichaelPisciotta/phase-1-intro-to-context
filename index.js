2df0e619008daac7fe51f22a7cd547e02058a4b1
MichaelPisciotta <mfp56@miami.edu>
2021-08-03 14:17:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+function createEmployeeRecord(array){
+    let record = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+    return record
+}
+
+
+function createEmployeeRecords(twoRows){
+    let record2 = createEmployeeRecord(twoRows)
+    return record2
+
+}
+
+
+
+
+
+function createTimeInEvent(){
+
+}
+
+function createTimeOutEvent(){
+
+}
+
+function hoursWorkedOnDate(){
+    
+}
+
+
+function wagesEarnedOnDate(){
+    
+}
+
+function allWagesFor(){
+    
+}
+
+function calculatePayroll(){
+    
+}
